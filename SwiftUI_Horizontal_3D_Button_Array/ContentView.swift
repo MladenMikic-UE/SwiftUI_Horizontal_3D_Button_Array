0775e1f04011fc8694ee76a50cc24ade32dcc6be
Mladen Mikic <mladen.mikic@rooom.com>
2024-04-25 22:07:37 +0200

    Initial Commit

---
+++ SwiftUI_Horizontal_3D_Button_Array/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Horizontal_3D_Button_Array
+//
+//  Created by MladenMikic# on 25.04.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Horizontal_3D_Button_Array/SwiftUI_Horizontal_3D_Button_ArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Horizontal_3D_Button_ArrayApp.swift
+//  SwiftUI_Horizontal_3D_Button_Array
+//
+//  Created by MladenMikic# on 25.04.2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Horizontal_3D_Button_ArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
